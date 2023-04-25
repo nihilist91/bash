1e93c741bcb4c4bbfb3d8748fa93b67d9d53e749
root <root@DESKTOP-LJHN55N.localdomain>
2023-04-25 13:34:31 +0100

    update

---
+++ install-kubectl.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Download the latest version of kubectl
+curl -LO "https://storage.googleapis.com/kubernetes-release/release/$(curl -s https://storage.googleapis.com/kubernetes-release/release/stable.txt)/bin/linux/amd64/kubectl"
+
+# Make the kubectl binary executable
+chmod +x ./kubectl
+
+# Move the binary to a directory in your PATH
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+# Verify the installation
+kubectl version --client
